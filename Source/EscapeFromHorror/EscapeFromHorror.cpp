9c222c5d2af4a31a29d1413e84f2be5680fd3a2b
Anyounguk <dksduddnr33@gmail.com>
2022-07-26 17:13:55 +0900

    Initial commit

---
+++ Source/EscapeFromHorror/EscapeFromHorror.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EscapeFromHorror.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EscapeFromHorror, "EscapeFromHorror" );
---
+++ Source/EscapeFromHorror/EscapeFromHorrorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeFromHorrorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEFROMHORROR_API AEscapeFromHorrorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
